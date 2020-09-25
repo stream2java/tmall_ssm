6eb0f3cb2cf383839070eaa155f3bf7886ffd98c
stream <stream2jazz@gmail.com>
2020-09-25 23:18:54 +0800

    update CategoryServiceImpl

--- src/main/java/com/stream/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/stream/tmall/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.stream.tmall.service.impl;
 
 import com.stream.tmall.mapper.CategoryMapper;
 import com.stream.tmall.pojo.Category;
+import com.stream.tmall.pojo.CategoryExample;
 import com.stream.tmall.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,26 +26,28 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> list() {
-        return categoryMapper.list();
+        CategoryExample example = new CategoryExample();
+        example.setOrderByClause("id desc");
+        return categoryMapper.selectByExample(example);
     }
 
     @Override
     public void add(Category category) {
-        categoryMapper.add(category);
+        categoryMapper.insert(category);
     }
 
     @Override
     public void delete(int id) {
-        categoryMapper.delete(id);
+        categoryMapper.deleteByPrimaryKey(id);
     }
 
     @Override
     public Category get(int id) {
-        return categoryMapper.get(id);
+        return categoryMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public void update(Category category) {
-        categoryMapper.update(category);
+        categoryMapper.updateByPrimaryKeySelective(category);
     }
 }
